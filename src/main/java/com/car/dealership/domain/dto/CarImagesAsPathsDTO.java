2e2d331d8fcddbd4846fed6b4b98f53d0dcb5fd7
nikolakirilov <@@>
2022-06-28 23:47:24 +0300

    New commit

---
+++ src/main/java/com/car/dealership/domain/dto/CarImagesAsPathsDTO.java
@@ -0,0 +1,139 @@
+package com.car.dealership.domain.dto;
+
+import java.util.List;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class CarImagesAsPathsDTO {
+
+    private String id;
+
+    private String modelName;
+
+    private String brand;
+
+    private String transmission;
+
+    private String type;
+
+    private String color;
+
+    private Integer manufactureYear;
+
+    private Integer emissionStandard;
+
+    private Integer horsePower;
+
+    private Integer truckCapacity;
+
+    private Double price;
+
+    private String dealership;
+
+    private List<String> pictures;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(Integer manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public Integer getEmissionStandard() {
+        return emissionStandard;
+    }
+
+    public void setEmissionStandard(Integer emissionStandard) {
+        this.emissionStandard = emissionStandard;
+    }
+
+    public Integer getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(Integer horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    public Integer getTruckCapacity() {
+        return truckCapacity;
+    }
+
+    public void setTruckCapacity(Integer truckCapacity) {
+        this.truckCapacity = truckCapacity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDealership() {
+        return dealership;
+    }
+
+    public void setDealership(String dealership) {
+        this.dealership = dealership;
+    }
+
+    public List<String> getPictures() {
+        return pictures;
+    }
+
+    public void setPictures(List<String> pictures) {
+        this.pictures = pictures;
+    }
+}
+
